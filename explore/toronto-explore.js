32097bf377c0313e5bdfbbf9d3cbed53e023ce57
Dimuth Kurukula <dimuthkurukula@gmail.com>
2021-02-08 00:03:29 -0500

    single and comma list parse functions

--- explore/toronto-explore.js
+++ explore/toronto-explore.js
@@ -16,10 +16,18 @@ const topKeys = (data) => {
 }	
 
 // get all unique values in data for given key
-const getAll = (data, key) => {
+const getAllCommaList = (data, key) => {
 	let all = data.map( d => {
 		let dk = d[`${key}`]
 		if (dk == null){ return "" } else { return dk.split(",") }
+	})
+	let unique = [...new Set(all.flat())].filter( x => x !== "")
+	return unique
+}
+const getAllSingle= (data, key) => {
+	let all = data.map( d => {
+		let dk = d[`${key}`]
+		if (dk == null){ return "" } else { return dk }
 	})
 	let unique = [...new Set(all.flat())].filter( x => x !== "")
 	return unique
@@ -28,6 +36,12 @@ const getAll = (data, key) => {
 let data = JSON.parse(fsdata.toString())
 let keys = topKeys(data)
 
-console.log(getAll(data,'topics'))
-
-
+console.log(getAllCommaList(data,'topics'))
+console.log(getAllCommaList(data,'formats'))
+console.log(getAllSingle(data,'isopen'))
+console.log(getAllSingle(data,'state'))
+console.log(getAllSingle(data,'type'))
+console.log(getAllSingle(data,'dataset_category'))
+console.log(getAllSingle(data,'refresh_rate'))
+console.log(getAllSingle(data,'owner_division'))
+console.log(getAllSingle(data,'private'))
